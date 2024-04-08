46274f7190e9ebed970d1443c9c9107e09671230
Freddy Panchi <freddy.panchi@gmail.com>
2024-04-08 11:44:39 -0500

    start footer

---
+++ src/Presentation/Components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col w-screen bg-[#000000C0]">
+      <div className="flex flex-col ">
+        <h5 className="poppins-bold text-white text-3xl">Contacto:</h5>
+      </div>
+      <div>
+        <h6 className="text-white">Hagamos algo grande juntos</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
